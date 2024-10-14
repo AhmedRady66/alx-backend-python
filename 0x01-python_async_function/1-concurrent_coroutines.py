1a43235e2f776a88c9a04a57cafebd38cea044b3
Ahmed Rady <rady20991@gmail.com>
2024-10-15 01:07:34 +0300

    Measure the runtime

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""File render list of random"""
+"""File that render float random numbers in list"""
 import asyncio
 from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Return list of float random numbers"""
+    """Return list of float numbers"""
     tasks = [wait_random(max_delay) for _ in range(n)]
-    result = await asyncio.gather(*tasks)
-    return result
+    return [await task for task in asyncio.as_completed(tasks)]
